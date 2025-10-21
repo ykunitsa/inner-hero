a50856735c4c9bed945dfbfa75d9ae39d1059201
Eugene Kunitsa <eugene.kunitsa@timspark.com>
2025-10-21 20:44:22 +0300

    Initial Commit

---
+++ Inner Hero/Inner_HeroApp.swift
@@ -0,0 +1,32 @@
+//
+//  Inner_HeroApp.swift
+//  Inner Hero
+//
+//  Created by Yauheni Kunitsa on 21.10.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Inner_HeroApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Inner HeroTests/Inner_HeroTests.swift
@@ -0,0 +1,17 @@
+//
+//  Inner_HeroTests.swift
+//  Inner HeroTests
+//
+//  Created by Yauheni Kunitsa on 21.10.25.
+//
+
+import Testing
+@testable import Inner_Hero
+
+struct Inner_HeroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
